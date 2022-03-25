b82571e7340058a6584df2c19a4ff24cf4abd0a9
Volodymyr <vshvets295@gmail.com>
2022-03-25 21:21:06 +0200

    Solved code mystery.

--- Space Rocks/src/javagames/util/Utility.java
+++ Space Rocks/src/javagames/util/Utility.java
@@ -23,8 +23,8 @@ public class Utility {
         float sy = (screenHeight - 1) / worldHeight;
         float tx = (screenWidth - 1) / 2.0f;
         float ty = (screenHeight - 1) / 2.0f;
-        Matrix3x3f viewport = Matrix3x3f.translate(sx, -sy);
-        viewport = viewport.mul(Matrix3x3f.scale(tx, ty));
+        Matrix3x3f viewport = Matrix3x3f.translate(-tx, -ty);
+        viewport = viewport.mul(Matrix3x3f.scale(sx, -sy));
         return viewport;
     }
 
